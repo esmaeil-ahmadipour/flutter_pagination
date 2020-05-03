96a3dfc44390ac5779961cea8841f80d84e1cd52
Esmaeil Ahmadipour <software8686@gmail.com>
2020-05-03 09:07:20 +0430

    commit> Add state

--- lib/ui/screens/list/list_state.dart
+++ lib/ui/screens/list/list_state.dart
@@ -1,6 +1,23 @@
-import 'package:meta/meta.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:flutterpagination/data/models/list_item.dart';
 
-@immutable
-abstract class ListState {}
+part 'list_state.g.dart';
 
-class InitialListState extends ListState {}
+abstract class ListState implements Built<ListState, ListStateBuilder> {
+  BuiltList<ListItem> get listItems;
+  bool get hasReachedEndOfResults;
+
+  ListState._();
+  factory ListState([updates(ListStateBuilder b) ]) = _$ListState;
+  factory ListState.initial(){
+    return ListState((b)=>b
+    ..listItems.replace(BuiltList<ListItem>())
+    ..hasReachedEndOfResults=false);
+  }
+  factory ListState.success(BuiltList<ListItem> items){
+    return ListState((b)=>b
+    ..listItems.replace(items)
+    ..hasReachedEndOfResults=false);
+  }
+}
