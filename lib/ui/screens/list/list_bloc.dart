3a7019a340125c511c7b71679b2da229b2bff167
Esmaeil Ahmadipour <software8686@gmail.com>
2020-05-03 09:51:18 +0430

    commit> Add Bloc / Add Base-ListPage

--- lib/ui/screens/list/list_bloc.dart
+++ lib/ui/screens/list/list_bloc.dart
@@ -1,15 +1,34 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutterpagination/data/generator/list_data_source.dart';
 import './bloc.dart';
 
 class ListBloc extends Bloc<ListEvent, ListState> {
-  @override
-  ListState get initialState => InitialListState();
+  final ListDataSource _dataSource;
+
+  ListBloc(this._dataSource);
+
+  void getNextListPage() {
+    add(FetchNextPage());
+  }
 
   @override
-  Stream<ListState> mapEventToState(
-    ListEvent event,
-  ) async* {
-    // TODO: Add Logic
+  ListState get initialState => ListState.initial();
+
+
+  @override
+  Stream<ListState> _bindEventsToStates(
+      ListState currentState, ListEvent event) async* {
+    if (event is FetchNextPage) {
+      try {
+        final nextPageItems = await _dataSource.getNextListPage();
+        yield ListState.success(currentState.listItems + nextPageItems);
+      } on NoNextPageException catch (_) {
+        yield currentState.rebuild((b) => b..hasReachedEndOfResults = true);
+      }
+    }
   }
+
+
 }
